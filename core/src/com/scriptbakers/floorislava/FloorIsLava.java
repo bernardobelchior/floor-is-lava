25d6e3a730ea4a0cd93b6f916a149eb5831f6d50
Bernardo Belchior <bernardo.belchior1@gmail.com>
2016-11-04 17:47:51 +0100

    Created game screen.

--- core/src/com/scriptbakers/floorislava/FloorIsLava.java
+++ core/src/com/scriptbakers/floorislava/FloorIsLava.java
@@ -5,29 +5,49 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
 
 public class FloorIsLava extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+	public static int WORLD_WIDTH = 1000;
+	public static int WORLD_HEIGHT = 2000;
+	public static Vector2 INITIAL_GRAVITY = new Vector2(0, -50);
+
+	World world;
+	GameScreen screen;
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		world = new World(INITIAL_GRAVITY, true);
+		screen = new GameScreen(world);
+
+		BodyDef bodyDef = new BodyDef();
+		bodyDef.position.set(WORLD_WIDTH/2, WORLD_HEIGHT/5);
+		bodyDef.type = BodyDef.BodyType.KinematicBody;
+		Body body = world.createBody(bodyDef);
+
+		CircleShape player = new CircleShape();
+		player.setRadius(WORLD_WIDTH/10);
+		body.createFixture(player, 1);
+
+		player.dispose();
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+
+		world.step(1/60f, 6, 2);
+		screen.render(1/60f);
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+
 	}
 }
