ba6116a7bc62127cccf124b2f9111d50f43de287
epassos26 <epassos26@gmail.com>
2016-11-04 16:12:23 +0100

    Added initial versions of Floor and Player classes

---
+++ core/src/com/scriptbakers/floorislava/gameentities/Floor.java
@@ -0,0 +1,13 @@
+package com.scriptbakers.floorislava.gameentities;
+
+/**
+ * Created by epassos on 11/4/16.
+ */
+
+public class Floor {
+
+    int speed;
+    boolean lava;
+
+    public Floor(){}
+}
