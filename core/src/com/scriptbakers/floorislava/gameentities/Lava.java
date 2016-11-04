9831aeae5e289bae04531d4ef4a1042f07369167
epassos26 <epassos26@gmail.com>
2016-11-04 16:19:47 +0100

    Added lava class

--- core/src/com/scriptbakers/floorislava/gameentities/Lava.java
+++ core/src/com/scriptbakers/floorislava/gameentities/Lava.java
@@ -4,9 +4,12 @@ package com.scriptbakers.floorislava.gameentities;
  * Created by epassos on 11/4/16.
  */
 
-public class Floor {
+public class Lava {
 
     int speed;
+    int length;
 
-    public Floor(){}
+    public Lava(int length){
+        this.length = length;
+    }
 }
