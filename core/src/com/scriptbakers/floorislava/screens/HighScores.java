606be4c73f55932a8b95f037d66fc4f9ca6fac5c
Inês Teixeira <inescfteixeira@gmail.com>
2016-11-05 04:30:09 +0000

    highscores class andoverride methods created

---
+++ core/src/com/scriptbakers/floorislava/screens/HighScores.java
@@ -0,0 +1,44 @@
+package com.scriptbakers.floorislava.screens;
+
+import com.badlogic.gdx.Screen;
+
+/**
+ * Created by inesc on 05/11/2016.
+ */
+
+public class HighScores implements Screen{
+    @Override
+    public void show() {
+        
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
